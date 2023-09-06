d16b31cc7f21049eda2aea42058eba36de861843
borgesmj <borgesmj19@gmail.com>
2023-09-06 12:49:54 -0500

    portfolio version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-// import './App.css'
+import './App.css'
 import Data from './Components/Data'
 import Profile from './Components/Profile'
 import Modal from './Components/Modal'
