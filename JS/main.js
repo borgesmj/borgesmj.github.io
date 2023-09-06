525aeeb4ebfec5cfc14d09c883832e40d8d2fb90
borgesmj <borgesmj19@gmail.com>
2023-09-06 12:36:59 -0500

    portfolio version 2

--- JS/main.js
+++
@@ -1,6 +0,0 @@
-function sendMail(){
-    let messageBody = document.getElementById("Emailtext").value;
-    let messageSubjetc =document.getElementById("Subject").value;
-    window.location.href = "mailto:borgesmj19@gmail.com?subject="+messageSubjetc+"&body="+messageBody;
-    
-}
