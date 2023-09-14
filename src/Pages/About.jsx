3c78e9c6a125d62f6c4612172209e716cd7461fa
borgesmj <borgesmj19@gmail.com>
2023-09-14 12:10:14 -0500

    importando about.css

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,3 +1,5 @@
+import './About.css'
+
 const About = ({ english }) => {
   const bio = [
     'Durante los últimos 5 años, he tenido la oportunidad de vivir en Colombia, específicamente en Medellín, Antioquia, junto con mi familia: mi esposa y nuestra hija de 5 años. Me gradué como Ingeniero Mecánico en 2015 y pronto me embarqué en mi primera experiencia laboral en una empresa petrolera, donde desempeñé el cargo de Inspector QA/QC durante dos años. // A mediados del año 2022, mi interés por el desarrollo web comenzó a florecer. A medida que 2022 llegaba a su fin, tomé la decisión de capacitarme en diversas áreas. En 2023, he invertido la mayor parte de mi tiempo en lecciones de YouTube y FreeCodeCamp, así como en la creación de pequeños proyectos para practicar. // Me he inclinado hacia el Front End debido a mi fascinación por el diseño y la interacción con el usuario, aunque también tengo un interés en las bases de datos. Mi objetivo a largo plazo es convertirme en un desarrollador FullStack. Aunque todavía estoy en proceso de aprendizaje, estoy emocionado por seguir descubriendo nuevas herramientas y tecnologías para mejorar mis habilidades en el desarrollo web.',
