80441681190dccc421aeecc726211ad8555eda14
borgesmj <borgesmj19@gmail.com>
2024-02-19 08:51:55 -0500

    responsive

--- src/Components/Tech_section.jsx
+++ src/Components/Tech_section.jsx
@@ -7,7 +7,7 @@ const Tech_section = () => {
   ]
   console.log(iconos)
   return(
-    <div className='my-2 py-2 w-full grid grid-cols-1 md:grid-cols-4 justify-items-center '>
+    <div className='my-2 py-2 w-full grid grid-cols-2 md:grid-cols-4 justify-items-center '>
         {iconos.map((item, index) => (
           <img key={`icono_${index}`} src={`/${item}.webp`} alt={`${item}_icono`} className="w-16 p-2 md:w-24 md:p-4 md:mx-2 lg:w-20 lg:p-2" />
         ) )}
