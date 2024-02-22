8869027a82faa4f114369a6bf3b54af18ed22d07
borgesmj <borgesmj19@gmail.com>
2024-02-22 11:45:57 -0500

    fixing responsive in large screens

--- src/Components/Infobar.jsx
+++ src/Components/Infobar.jsx
@@ -2,7 +2,7 @@ const Infobar = ({allPosts}) => {
 
   
   return (
-    <section className='hidden lg:block lg:fixed lg:top-0 lg:bottom-0 lg:px-8 lg:py-4lg:right-16 lg:z-50 lg:h-9/10 lg:bg-background-300 lg:border-l-white lg:w-[13rem] xl:max-w-56 2xl:max-w-64 xl:right-28 2xl:right-60 '>
+    <section className='hidden lg:block lg:fixed lg:top-0 lg:bottom-0 lg:px-8 lg:py-4 lg:right-16 lg:z-50 lg:h-9/10 lg:bg-background-300 lg:border-l-white lg:w-[13rem] xl:max-w-56 2xl:max-w-64 xl:right-28 2xl:right-60 '>
       <div className="w-full">
         <h2>Last posts:</h2>
       <ul>
