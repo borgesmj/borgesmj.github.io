06894cf0b2f25e9779a71a3281483e5a0ab655f6
borgesmj <borgesmj19@gmail.com>
2024-02-22 12:53:52 -0500

    italic font text title page

--- src/Components/LastPosts.jsx
+++ src/Components/LastPosts.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const LastPosts = ({allPosts}) => {
   return (
     <div className="w-full flex flex-col">
-      <h2>Last posts on my blog:</h2>
+      <h2 className="w-full text-center text-[16px] font-bold">Last posts on my blog:</h2>
       <ul className="mb-2">
         {allPosts.slice(0, 4).map((post, index) => (
-          <li className="lg:p-0 lg:w-full lg:h-full lg:text-[14px] lg:flex lg:items-center -m-4 mt-1 mb-1">
+          <li className="lg:p-0 lg:w-full lg:h-full lg:text-[14px] lg:flex lg:items-center m-0 mt-1 mb-1">
             <a className="lg:hover:underline" href={post.url}>
               {index + 1}. {post.frontmatter.title}
             </a>
