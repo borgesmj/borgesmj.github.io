06310b2a86401d16b784b57e1d58b00840ae7685
borgesmj <borgesmj19@gmail.com>
2024-02-22 11:43:49 -0500

    fixing responsive in large screens

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="navbar bg-background-100 w-dvw text-white py-2 px-1 bottom-0 left-0 border-t-[1px] border-t-white border-t-solid z-50 fixed md:left-12 md:h-full md:w-fit md:top-0 md:bottom-8 md:border-r-[0px] md:border-r-solid md:border-r-none md:border-t-[0px] md:flex md:flex-col md:justify-center lg:left-16  font-mono xl:left-20 2xl:left-60 md:bg-background-300">
+    <nav className="navbar bg-background-100 w-dvw text-white py-2 px-1 bottom-0 left-0 border-t-[1px] border-t-white border-t-solid z-50 fixed md:left-12 md:h-full md:w-[13rem] md:top-0 md:bottom-8 md:border-r-[0px] md:border-r-solid md:border-r-none md:border-t-[0px] md:flex md:flex-col md:justify-center lg:left-16  font-mono xl:left-28 2xl:left-60 md:bg-background-300">
       <ul className=" flex flex-row w-full h-full justify-between md:flex-col md:justify-around px-0 md:h-1/4 lg:h-1/3">
         {navLinks.map((item, index) => (
           <li key={index} className=" flex justify-center items-center md:p-2 w-1/5 md:border-t-white md:border-t-solid md:border-t-[1px] md:w-full md:h-1/5 lg:relative">
