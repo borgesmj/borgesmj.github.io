12266bdcb24a9b3c4270e2f3d28b290ebcdaceea
borgesmj <borgesmj19@gmail.com>
2024-02-22 12:15:24 -0500

    adding last post section

--- src/Components/Infobar.jsx
+++ src/Components/Infobar.jsx
@@ -1,18 +1,11 @@
+import LastPosts from "./LastPosts"
+
 const Infobar = ({allPosts}) => {
 
   
   return (
-    <section className='hidden lg:block lg:fixed lg:top-0 lg:bottom-0 lg:px-8 lg:py-4 lg:right-16 lg:z-50 lg:h-9/10 lg:bg-background-300 lg:border-l-white lg:w-[13rem] xl:max-w-56 2xl:max-w-64 xl:right-28 2xl:right-60 '>
-      <div className="w-full">
-        <h2>Last posts:</h2>
-      <ul>
-        {
-          allPosts.slice(0,4).map((post) => (
-            <li className="list-disc"><a href={post.url}>{post.frontmatter.title}</a></li>
-          ))
-        }
-      </ul>
-      </div>
+    <section className='hidden lg:block lg:fixed lg:top-0 lg:bottom-0 lg:pl-8 lg:pr-4 lg:pb-4 lg:pt-16 lg:right-16 lg:z-50 lg:h-9/10 lg:bg-background-300 lg:border-l-white lg:w-[13rem] xl:max-w-56 2xl:max-w-64 xl:right-28 2xl:right-60 '>
+      <LastPosts allPosts = {allPosts}/>
     </section>
   )
 }
---
+++ src/Components/LastPosts.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LastPosts = ({allPosts}) => {
+  return (
+    <div className="w-full flex flex-col">
+      <h2>Last posts on my blog:</h2>
+      <ul className="mb-2">
+        {allPosts.slice(0, 4).map((post, index) => (
+          <li className="lg:p-0 lg:w-full lg:h-full lg:text-[14px] lg:flex lg:items-center -m-4 mt-1 mb-1">
+            <a className="lg:hover:underline" href={post.url}>
+              {index + 1}. {post.frontmatter.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <a href="/blog" className="text-[10px] underline self-end">
+        View all posts
+      </a>
+    </div>
+  );
+};
+
+export default LastPosts;
