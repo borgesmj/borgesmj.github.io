f47a2af6e7a21abff20ebb279509c3de9b776ff7
borgesmj <borgesmj19@gmail.com>
2024-02-23 14:51:09 -0500

    reversed order posts

--- src/Components/LastPosts.jsx
+++ src/Components/LastPosts.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const LastPosts = ({ allPosts }) => {
-  const [orderedPosts, setOrderedPosts] = useState([]);
-  let lastPosts = allPosts.slice(0, 4).reverse();
-  setOrderedPosts(lastPosts);
+
+  let fivePosts = allPosts.slice(0,4)
+  let lastPosts = fivePosts.reverse()
+  
   return (
     <div className="w-full flex flex-col border-y-solid border-y-[1px] border-y-white py-2">
       <h2 className="w-full text-center text-[16px] font-bold">
         Last posts on my blog:
       </h2>
       <ul className="mb-2">
-        {orderedPosts.map((post, index) => (
+        {lastPosts.map((post, index) => (
           <li className="lg:p-0 lg:w-full lg:h-full lg:text-[14px] lg:flex lg:items-center m-0 mt-1 mb-1">
             <a className="lg:hover:underline" href={post.url}>
               {index + 1}. {post.frontmatter.title}
