40d768ab51763c45ec2505901d622e20ae5bf09a
borgesmj <borgesmj19@gmail.com>
2024-02-26 14:23:11 -0500

    changing the home/profile page

---
+++ src/Components/Profile/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full h-20'>
+      <img src="/blog-images/post-1.webp" alt="" />
+      <img src="https://avatars.githubusercontent.com/u/121818423?v=4" alt="" className='h-24 w-auto rounded-full border-solid border-accent-200 border-[2px] -mt-10 z-50 ml-4 absolute md:h-32 md:-mt-14' />
+    </div>
+  )
+}
+
+export default Header
