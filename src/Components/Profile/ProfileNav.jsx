5c98dc03fa8073a188479516e84a3ea0a75813f0
borgesmj <borgesmj19@gmail.com>
2024-02-27 15:47:18 -0500

    stilynf link, active and pending and the navbar

--- src/Components/Profile/ProfileNav.jsx
+++ src/Components/Profile/ProfileNav.jsx
@@ -16,20 +16,20 @@ const ProfileNav = () => {
     },
     {
       href: "/work-together/",
-      name: "Work with me",
+      name: "Work",
     },
   ];
   return (
-    <ul className="absolute top-[27rem] flex flex-row w-fit">
+    <ul className="absolute flex justify-between items-center top-[27rem] profile-nav border-b-solid border-b-[1px] border-b-accent-100 w-dvw md:w-[55%] lg:w-[45%] 2xl:w-[42%] 2xl:top-[30rem]">
       {profileNavLinks.map((item, index) => (
         <li
           key={`pnl_${index}`}
-          className="px-4 w-fit"
+          className="px-4 mx-2 w-fit h-full flex justify-center items-center"
         >
           <NavLink
             to={item.href}
             className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "bg-[#ff0000]" : ""
+              isPending ? "h-full w-fit flex justify-center items-center bg-[#ffffff]" : isActive ? "h-full w-fit  flex justify-center items-center border-b-[4px] border-b-solid border-b-primary-300" : "h-full w-fit flex justify-center items-center border-b-[4px] border-b-solid border-b-transparent"
             }
           >
             {item.name}
