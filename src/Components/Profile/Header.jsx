0fe0c25f4b1092e9e457d14d8fc5a6c1ad690b73
borgesmj <borgesmj19@gmail.com>
2024-02-27 16:04:07 -0500

    changing header image heigth

--- src/Components/Profile/Header.jsx
+++ src/Components/Profile/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='w-full h-20'>
-      <img src="/blog-images/post-1.webp" alt="" className='aspect-video w-dvw h-40 md:h-48 md:w-full' />
+      <img src="/blog-images/post-1.webp" alt="" className='aspect-video w-dvw h-40 md:h-48 md:w-full xl:h-[14rem] 2xl:h-[15rem]' />
       <img src="https://avatars.githubusercontent.com/u/121818423?v=4" alt="" className='h-24 w-auto rounded-full border-solid border-accent-200 border-[2px] -mt-10 z-50 ml-4 absolute md:h-32 md:-mt-14' />
     </div>
   )
--- src/Components/Profile/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo.jsx
@@ -2,7 +2,7 @@ import {MapPin} from "../../Icons/Icons"
 
 const ProfileInfo = ({location, bio}) => {
   return (
-    <div className='w-dvw absolute top-[16rem] p-4 md:top-[16rem] md:p-8 md:w-[65%] lg:top-[17rem] lg:w-[45%] lg:p-4 xl:w-1/2 xl:top-[17rem] 2xl:w-2/5 2xl:top-[20rem]'>
+    <div className='w-dvw absolute top-[14rem] p-4 md:top-[16rem] md:p-8 md:w-[65%] lg:top-[17rem] lg:w-[45%] lg:p-4 xl:w-1/2 xl:top-[19rem] 2xl:w-2/5 2xl:top-[22rem]'>
       <h2 className='font-bold text-xl md:text-[1.5rem] '>Miguel José</h2>
       <h3><a href="https://github.com/borgesmj" target='_blank' className='font-bold border-b-[1px] border-b-white border-b-solid'>@Borgesmj</a> on Github</h3>
       <p className='text-[1rem]'>{bio}</p>
