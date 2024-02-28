86807841aada4e82e826752244e91d8b6c9fb13d
borgesmj <borgesmj19@gmail.com>
2024-02-28 14:35:47 -0500

    changing position navbar. CHanging 'work' tab to 'lets work' tab

--- src/Components/Profile/ProfileNav.jsx
+++ src/Components/Profile/ProfileNav.jsx
@@ -12,7 +12,7 @@ const ProfileNav = () => {
     },
     {
       href: "/work-together/",
-      name: "Work",
+      name: "Let's work",
     },
     {
       href: "/files/",
@@ -20,7 +20,7 @@ const ProfileNav = () => {
     },
   ];
   return (
-    <ul className="absolute flex justify-between items-center top-[27rem] profile-nav border-b-solid border-b-[1px] border-b-accent-100 w-dvw md:w-[55%] lg:w-[45%] 2xl:w-[41%] 2xl:top-[30rem]">
+    <ul className="absolute flex justify-between items-center top-[27rem] profile-nav border-b-solid border-b-[1px] border-b-accent-100 w-full md:w-[55%] lg:w-[45%] 2xl:w-[41%] 2xl:top-[30rem]">
       {profileNavLinks.map((item, index) => (
         <li
           key={`pnl_${index}`}
@@ -29,7 +29,7 @@ const ProfileNav = () => {
           <NavLink
             to={item.href}
             className={({ isActive, isPending }) =>
-              isPending ? "h-full w-fit flex justify-center items-center bg-[#ffffff]" : isActive ? "h-full w-fit  flex justify-center items-center border-b-[4px] border-b-solid border-b-primary-300 text-white" : "h-full w-fit flex justify-center items-center border-b-[4px] border-b-solid border-b-transparent text-accent-100"
+              isPending ? "h-full w-20 flex justify-center items-center bg-[#ffffff]" : isActive ? "h-full w-20 flex justify-center items-center border-b-[4px] border-b-solid border-b-primary-300 text-white" : "h-full w-20 flex justify-center items-center border-b-[4px] border-b-solid border-b-transparent text-accent-100"
             }
           >
             {item.name}
