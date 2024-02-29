33a2bd0b85059fdd5b8783378a5c7f4c38033cee
borgesmj <borgesmj19@gmail.com>
2024-02-29 10:19:47 -0500

    setting gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://borgesmj.github.io/',
+  site: 'https://borgesmj.github.io',
+  base: "/"
 });
