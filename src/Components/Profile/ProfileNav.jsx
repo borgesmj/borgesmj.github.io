81245c7f151151a61e8a99b0d41417ffd003fde3
borgesmj <borgesmj19@gmail.com>
2024-02-29 09:39:47 -0500

    reducing size of profile nav

--- src/Components/Profile/ProfileNav.jsx
+++ src/Components/Profile/ProfileNav.jsx
@@ -24,12 +24,12 @@ const ProfileNav = () => {
       {profileNavLinks.map((item, index) => (
         <li
           key={`pnl_${index}`}
-          className="px-4 mx-2 w-fit h-full flex justify-center items-center"
+          className="px-4 mx-2 w-fit h-full flex justify-center items-center md:mx-0 md:px-2"
         >
           <NavLink
             to={item.href}
             className={({ isActive, isPending }) =>
-              isPending ? "h-full w-20 flex justify-center items-center bg-[#ffffff] text-center" : isActive ? "h-full w-20 flex justify-center items-center border-b-[4px] border-b-solid border-b-primary-300 text-white text-center" : "h-full w-20 flex justify-center items-center border-b-[4px] border-b-solid border-b-transparent text-accent-100 text-center"
+              isPending ? "h-full w-20 flex justify-center items-center bg-[#ffffff] text-center text-xs" : isActive ? "h-full w-20 flex justify-center items-center border-b-[4px] border-b-solid border-b-primary-300 text-white text-center text-xs" : "h-full w-20 flex justify-center items-center border-b-[4px] border-b-solid border-b-transparent text-accent-100 text-center text-xs"
             }
           >
             {item.name}
