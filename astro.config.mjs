ce66d2308fb59e264e804c819518aedb0acda293
borgesmj <borgesmj19@gmail.com>
2024-02-29 12:54:03 -0500

    setting back to the previous chamges before gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://borgesmj.github.io',
-  base: "/"
+  integrations: [tailwind(), react()]
 });
