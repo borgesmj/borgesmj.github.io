87413b0f6f17cda144b2e13a4eb71b97172e8c40
borgesmj <borgesmj19@gmail.com>
2024-03-01 13:30:41 -0500

    changing zise of the icons. Convert project name to a string

--- src/Icons/Icons.jsx
+++ src/Icons/Icons.jsx
@@ -74,8 +74,8 @@ export const Code_Icon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
+      width="16"
+      height="16"
       fill="#FFFFFF"
       viewBox="0 0 256 256"
     >
@@ -88,8 +88,8 @@ export const Play_Icon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
+      width="16"
+      height="16"
       fill="#FFFFFF"
       viewBox="0 0 256 256"
     >
