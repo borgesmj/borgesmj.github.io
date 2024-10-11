85b89b3d164b2402e6755a1d1b96829f0b3a61a1
borgesmj19 <borgesmj19@gmail.com>
2024-10-11 16:04:51 +0000

    portfolio new version

---
+++ src/data/data.ts
@@ -0,0 +1,40 @@
+export const navlinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "Service",
+    href: "#service",
+  },
+  {
+    name: "Résumé",
+    href: "#resume",
+  },
+  {
+    name: "Projects",
+    href: "#projects",
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+  },
+];
+
+export const profile = {
+  name: "MIguel José",
+  titulo: "FrontEnd Web developer",
+  available: false,
+};
+
+export const socials = [
+  { name: "github", href: "#" },
+  { name: "linkedin", href: "#" },
+  { name: "twitter", href: "#" },
+  { name: "instagram", href: "#" },
+  { name: "telegram", href: "#" },
+];
