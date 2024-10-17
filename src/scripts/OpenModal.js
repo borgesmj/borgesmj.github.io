5283f3b006300d1ccdc2b176552f3f6f767f6c51
borgesmj19 <borgesmj19@gmail.com>
2024-10-17 23:08:03 +0000

    modal

--- src/scripts/OpenModal.js
+++ src/scripts/OpenModal.js
@@ -11,8 +11,33 @@ buttonsList.forEach((button) => {
     modal.classList.add("scale-in-center");
     modal.classList.remove("hidden");
     modal.classList.add("flex");
-    const selectdWorkExperience = workExperience[experienceId - 1];
-    document.getElementById('modal-content').innerText = selectdWorkExperience.position
+    const filteredWorkExperience = workExperience[experienceId - 1];
+    document.getElementById("modal-content").innerHTML = `
+    
+    <h2 id="work-title" class="text-[30px] font-[600] lg:text-[40px]">${filteredWorkExperience.position}</h2>
+    <p id="work-company" class="text-[30px]">${filteredWorkExperience.company}</p>
+    <span>Since: <strong>${filteredWorkExperience.startDate}</strong></span>
+    <span>Until: ${filteredWorkExperience.endDate}</span>
+    <span class="flex flex-row"
+      ><Icon name="map-pin" color="black" /> ${filteredWorkExperience.location}</span
+    >
+    <p>Responsabilities:</p>
+    <ul class="list-disc pl-10">
+      ${
+        filteredWorkExperience.responsibilities.map((responsibility) => {
+          return `<li>${responsibility}</li>`
+        }).join('')
+      }
+    </ul>
+    <p>Technologies:</p>
+    <p class="flex flex-wrap flex-row justify-center gap-4">
+      ${
+        filteredWorkExperience.technologies.map((tech) => {
+          return `<span>${tech}</span>`
+        }).join('')
+      }
+    </p>
+    `;
 
     setTimeout(() => {
       modal.classList.remove("scale-in-center");
