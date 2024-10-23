49bbb9ae3ac145f8412ef6d478bc9af735ca0476
borgesmj19 <borgesmj19@gmail.com>
2024-10-23 14:06:11 +0000

    tooltip

--- src/data/data.ts
+++ src/data/data.ts
@@ -25,33 +25,38 @@ export const profile = {
   name: "Miguel José",
   role: "FrontEnd Web Developer",
   available: true,
-  bio: "**Lorem ipsum dolor sit amet**, consectetur adipisicing elit. Assumenda itaque eligendi aliquid alias quibusdam, sequi rem totam amet blanditiis dolorem a, non, dignissimos molestiae autem odio soluta fugiat? || Voluptatibus facilis eveniet facere dolorem sunt nulla laboriosam temporibus voluptatem blanditiis commodi repellendus eligendi vel, molestias cumque at totam perspiciatis deserunt quisquam debitis rerum laudantium!"
+  bio: "**Lorem ipsum dolor sit amet**, consectetur adipisicing elit. Assumenda itaque eligendi aliquid alias quibusdam, sequi rem totam amet blanditiis dolorem a, non, dignissimos molestiae autem odio soluta fugiat? || Voluptatibus facilis eveniet facere dolorem sunt nulla laboriosam temporibus voluptatem blanditiis commodi repellendus eligendi vel, molestias cumque at totam perspiciatis deserunt quisquam debitis rerum laudantium!",
 };
 
 export const socials = [
-  { name: "github", href: "#" },
-  { name: "linkedin", href: "#" },
-  { name: "twitter", href: "#" },
-  { name: "instagram", href: "#" },
-  { name: "telegram", href: "#" },
+  { name: "github", href: "#", color: "#171515" },
+  { name: "linkedin", href: "#", color: "#0073b1" },
+  { name: "twitter", href: "#", color: "#1DA1F2" },
+  { name: "instagram", href: "#", color: "#E1306C" },
+  { name: "telegram", href: "#", color: "#229ED9" },
 ];
 
 export const workExperience = [
   {
     id: 1,
-    company: "Tech Corp",           // Nombre de la empresa
+    company: "Tech Corp", // Nombre de la empresa
     position: "Frontend Developer", // Cargo desempeñado
-    startDate: "January 2022",      // Fecha de inicio
-    endDate: "Present",             // Fecha de finalización (o "Present" si sigues trabajando allí)
-    responsibilities: [             // Lista de responsabilidades o logros
+    startDate: "January 2022", // Fecha de inicio
+    endDate: "Present", // Fecha de finalización (o "Present" si sigues trabajando allí)
+    responsibilities: [
+      // Lista de responsabilidades o logros
       "Developed responsive web applications using React and Tailwind CSS.",
       "Collaborated with backend team to integrate REST APIs.",
-      "Led UI/UX improvements that enhanced user experience by 20%."
+      "Led UI/UX improvements that enhanced user experience by 20%.",
     ],
-    location: "Remote",             // Ubicación (puede ser física o remoto)
-    technologies: [                 // Tecnologías usadas
-      "JavaScript", "React", "Tailwind CSS", "REST APIs"
-    ]
+    location: "Remote", // Ubicación (puede ser física o remoto)
+    technologies: [
+      // Tecnologías usadas
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "REST APIs",
+    ],
   },
   {
     id: 2,
@@ -62,12 +67,10 @@ export const workExperience = [
     responsibilities: [
       "Assisted in the development of client websites using HTML, CSS, and JavaScript.",
       "Implemented SEO best practices to increase website visibility.",
-      "Supported team in debugging and testing web applications."
+      "Supported team in debugging and testing web applications.",
     ],
     location: "New York, NY",
-    technologies: [
-      "HTML", "CSS", "JavaScript", "Git"
-    ]
+    technologies: ["HTML", "CSS", "JavaScript", "Git"],
   },
   {
     id: 3,
@@ -78,70 +81,72 @@ export const workExperience = [
     responsibilities: [
       "Assisted in the development of client websites using HTML, CSS, and JavaScript.",
       "Implemented SEO best practices to increase website visibility.",
-      "Supported team in debugging and testing web applications."
+      "Supported team in debugging and testing web applications.",
     ],
     location: "New York, NY",
-    technologies: [
-      "HTML", "CSS", "JavaScript", "Git"
-    ]
-  }
+    technologies: ["HTML", "CSS", "JavaScript", "Git"],
+  },
 ];
 
 export const projects = [
-    {
-      "id": 1,
-      "name": "ChatMob",
-      "description": "A real-time chat application built using Firebase for backend services and React for the frontend. The app supports user authentication, message storage, and real-time updates.",
-      "technologies": ["React", "Firebase", "Tailwind CSS", "JavaScript"],
-      "repositoryLink": "https://github.com/borgesmj/real-time-chat-app",
-      "liveDemo": "https://chatmob.netlify.app/",
-      "year": 2024,
-      "features": [
-        "Real-time messaging",
-        "Firebase authentication",
-        "Responsive design"
-      ]
-    },
-    {
-      "id": 2,
-      "name": "Sweet Bites",
-      "description": "An e-commerce platform for sweets and snacks built with React Router. It includes features such as product catalog, shopping cart, and user reviews.",
-      "technologies": ["React Router", "Tailwind CSS", "Firebase", "React"],
-      "repositoryLink": "https://github.com/borgesmj/sweet-bites",
-      "liveDemo": "https://sweet-bites.co/",
-      "year": 2023,
-      "features": [
-        "Product catalog",
-        "Secure payment integration",
-        "Shopping cart functionality"
-      ]
-    },
-    {
-      "id": 3,
-      "name": "Frontend 101",
-      "description": "A blog that focuses on teaching the fundamentals of frontend development with tutorials on React, JavaScript, and other frontend technologies.",
-      "technologies": ["Astro", "React", "Tailwind CSS"],
-      "repositoryLink": "https://github.com/borgesmj/saburo-blog",
-      "liveDemo": "https://frontend101.netlify.app/",
-      "year": 2024,
-      "features": [
-        "Tutorials on React and JavaScript",
-        "SEO-friendly static site",
-        "Responsive layout"
-      ]
-    },
-    {
-      "id": 4,
-      "name": "SaburoFinanceBot",
-      "description": "A Telegram bot built with Java and MySQL that tracks user expenses and income. It features user-friendly commands for adding, updating, and viewing financial data.",
-      "technologies": ["Java", "MySQL", "Telegram API"],
-      "repositoryLink": "https://github.com/borgesmj/telegram_app_bot",
-      "liveDemo": null,
-      "year": 2024,
-      "features": [
-        "Expense and income tracking",
-        "MySQL database integration",
-        "Interactive user commands"
-      ]
-    }
-  ]
+  {
+    id: 1,
+    name: "ChatMob",
+    description:
+      "A real-time chat application built using Firebase for backend services and React for the frontend. The app supports user authentication, message storage, and real-time updates.",
+    technologies: ["React", "Firebase", "Tailwind CSS", "JavaScript"],
+    repositoryLink: "https://github.com/borgesmj/real-time-chat-app",
+    liveDemo: "https://chatmob.netlify.app/",
+    year: 2024,
+    features: [
+      "Real-time messaging",
+      "Firebase authentication",
+      "Responsive design",
+    ],
+  },
+  {
+    id: 2,
+    name: "Sweet Bites",
+    description:
+      "An e-commerce platform for sweets and snacks built with React Router. It includes features such as product catalog, shopping cart, and user reviews.",
+    technologies: ["React Router", "Tailwind CSS", "Firebase", "React"],
+    repositoryLink: "https://github.com/borgesmj/sweet-bites",
+    liveDemo: "https://sweet-bites.co/",
+    year: 2023,
+    features: [
+      "Product catalog",
+      "Secure payment integration",
+      "Shopping cart functionality",
+    ],
+  },
+  {
+    id: 3,
+    name: "Frontend 101",
+    description:
+      "A blog that focuses on teaching the fundamentals of frontend development with tutorials on React, JavaScript, and other frontend technologies.",
+    technologies: ["Astro", "React", "Tailwind CSS"],
+    repositoryLink: "https://github.com/borgesmj/saburo-blog",
+    liveDemo: "https://frontend101.netlify.app/",
+    year: 2024,
+    features: [
+      "Tutorials on React and JavaScript",
+      "SEO-friendly static site",
+      "Responsive layout",
+    ],
+  },
+  {
+    id: 4,
+    name: "SaburoFinanceBot",
+    description:
+      "A Telegram bot built with Java and MySQL that tracks user expenses and income. It features user-friendly commands for adding, updating, and viewing financial data.",
+    technologies: ["Java", "MySQL", "Telegram API"],
+    repositoryLink: "https://github.com/borgesmj/telegram_app_bot",
+    liveDemo: null,
+    year: 2024,
+    features: [
+      "Expense and income tracking",
+      "MySQL database integration",
+      "Interactive user commands",
+    ],
+  },
+];
